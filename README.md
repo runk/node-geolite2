c9578b79fd41469d7ad6865f2a781c0e698df406
ALMaclaine <almaclaine@gmail.com>
2020-09-03 22:12:14 -0700

    Add functionality to select which dbs will download

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Maxmind's GeoLite2 Free Databases download helper.
 
 ## Configuration
 
+### Access Key
+
 **IMPORTANT** You must setup `MAXMIND_LICENSE_KEY` environment variable be able to download databases. To do so, go to the https://www.maxmind.com/en/geolite2/signup, create a free account and generate new license key.
 
 If you don't have access to the environment variables during installation, you can provide license key via `package.json`:
@@ -23,6 +25,25 @@ If you don't have access to the environment variables during installation, you c
 ```
 
 Beware of security risks of adding keys and secrets to your repository!
+
+### Selecting databases to download
+
+You can select the dbs you want downloaded by adding a `selected-dbs` property on `geolite2` via `package.json`.
+
+`selected-dbs` must be an array of strings, one or more of the values `City`, `Country`, `ASN`.
+
+If `selected-dbs` is unset, or is set but empty, all dbs will be downloaded.
+
+```jsonc
+{
+  ...
+  "geolite2": {
+    "selected-dbs": ["City", "Country", "ASN"]
+  }
+  ...
+}
+```
+
 
 ## Usage
 
--- test/index.js
+++ test/index.js
@@ -2,22 +2,31 @@ const assert = require('assert');
 const fs = require('fs');
 const geolite2 = require('../');
 
+const {selectedDbs} = require('../utils');
+const selected = selectedDbs();
+
 describe('geolite2', function() {
-  it('should return a valid city db path', function() {
-    var stat = fs.statSync(geolite2.paths.city);
-    assert(stat.size > 1e6);
-    assert(stat.ctime);
-  });
+  if(selected.includes('City')) {
+    it('should return a valid city db path', function () {
+      var stat = fs.statSync(geolite2.paths.city);
+      assert(stat.size > 1e6);
+      assert(stat.ctime);
+    });
+  }
 
-  it('should return a valid country db path', function() {
-    var stat = fs.statSync(geolite2.paths.country);
-    assert(stat.size > 1e6);
-    assert(stat.ctime);
-  });
+  if(selected.includes('Country')) {
+    it('should return a valid country db path', function () {
+      var stat = fs.statSync(geolite2.paths.country);
+      assert(stat.size > 1e6);
+      assert(stat.ctime);
+    });
+  }
 
-  it('should return a valid ASN db path', function() {
-    var stat = fs.statSync(geolite2.paths.asn);
-    assert(stat.size > 1e6);
-    assert(stat.ctime);
-  });
+  if(selected.includes('ASN')) {
+    it('should return a valid ASN db path', function () {
+      var stat = fs.statSync(geolite2.paths.asn);
+      assert(stat.size > 1e6);
+      assert(stat.ctime);
+    });
+  }
 });
